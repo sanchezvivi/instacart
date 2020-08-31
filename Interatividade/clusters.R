522f5f0669514f30bf11cfeb6a78772f0fc0d3c5
Viviane Sanchez <vivi.c.sanchez@gmail.com>
2020-08-31 08:29:04 -0300

    interatividade

--- Interatividade/clusters.R
+++ Interatividade/clusters.R
@@ -1,68 +1,49 @@
 
 # Bibliotecas -------------------------------------------------------------
 
-# biblios <- c('tidyverse','dplyr', 'ggplot2', 'lubridate', 'stringr', 
-#              'inspectdf', 'skimr', 'naniar', 'visdat', 'tidymodels', 
-#              'klaR', 'corrplot', 'NetCluster', 'factoextra', 'maptree', 'treemap', 'DT','patchwork')
-# library(ggiraph)
-# library(tidymodels)
-# library(tidyverse)
-# library(cluster)
 
-biblios <- c('stringr', 'inspectdf', 'dplyr', 'skimr', 
-             'factoextra', 'treemap', 'd3treeR', 'ggiraphExtra')
-
-
-for (i in biblios){
-  if(!require(i, character.only = TRUE)){install.packages(paste0(i)); library(i, character.only = TRUE)}
-}
-
-
-
-# install.packages("remotes")
-#remotes::install_github("timelyportfolio/d3treeR")
-# 
-# install.packages('treemap')
+library(tidymodels)
+library(tidyverse)
+library(inspectdf)
+library(skimr)
 library(treemap)
- 
 library(d3treeR)
-# 
-# library(ggiraphExtra)
+library(ggiraphExtra)
 
 
 # Importando os dados em .csv, usando o read.csv --------------------------
 
-path <- "data\\"
-# path <- "..\\..\\data\\"
-file_aisles <- "aisles.csv"
-base_aisles <- read.csv(paste(path,file_aisles,sep = ""))
-
-file_dept <- "departments.csv"
-base_dept <- read.csv(paste(path,file_dept,sep = ""))
-
-file_ord_prior <- "order_products__prior.csv"
-base_ord_prior <- read.csv(paste(path,file_ord_prior,sep = "")) %>% glimpse()
-
-file_ord_train <- "order_products__train.csv"
-base_ord_train <- read.csv(paste(path,file_ord_train,sep = "")) %>% glimpse()
-
-file_orders <- "orders.csv"
-base_orders <- read.csv(paste(path,file_orders,sep = "")) %>% glimpse()
-
-file_products <- "products.csv"
-base_products <- read.csv(paste(path,file_products,sep = "")) %>% glimpse()
-
-
-
-# Funções -----------------------------------------------------------------
-
-x4 <- function(x) x^4
-
-x_4<- function(x) sqrt(sqrt(x))
-
-x2 <- function(x) x^2
-
-x_2<- function(x) sqrt(x)
+#path <- "data/"
+## path <- "..\\..\\data\\"
+#file_aisles <- "aisles.csv"
+#base_aisles <- read.csv(paste(path,file_aisles,sep = ""))
+#
+#file_dept <- "departments.csv"
+#base_dept <- read.csv(paste(path,file_dept,sep = ""))
+#
+#file_ord_prior <- "order_products__prior.csv"
+#base_ord_prior <- read.csv(paste(path,file_ord_prior,sep = "")) %>% glimpse()
+#
+#file_ord_train <- "order_products__train.csv"
+#base_ord_train <- read.csv(paste(path,file_ord_train,sep = "")) %>% glimpse()
+#
+#file_orders <- "orders.csv"
+#base_orders <- read.csv(paste(path,file_orders,sep = "")) %>% glimpse()
+#
+#file_products <- "products.csv"
+#base_products <- read.csv(paste(path,file_products,sep = "")) %>% glimpse()
+#
+#
+#
+## Funções -----------------------------------------------------------------
+#
+#x4 <- function(x) x^4
+#
+#x_4<- function(x) sqrt(sqrt(x))
+#
+#x2 <- function(x) x^2
+#
+#x_2<- function(x) sqrt(x)
 
 
 # Inicio Código -----------------------------------------------------------
@@ -89,7 +70,10 @@ base_orders_cl <- base_orders_cl %>% right_join(usuarios_10 %>% select(user_id))
 base_ord_geral <- base_ord_geral %>% right_join(base_orders_cl %>% select(order_id)) 
 
 # Fazendo um left join da base de 'base_prod' com a base de base_aisles e base_dept, para trazer os nomes dos corredores e departamentos
-base_products_names <- base_products %>% left_join(base_aisles) %>% left_join(base_dept)
+base_products_names <- base_products %>% 
+                      left_join(base_aisles) %>% 
+                      left_join(base_dept)
+
 base_products_names <- base_products_names[,c(1:2,5:6)]
 
 
@@ -157,7 +141,7 @@ base_k_user_ord <- base_ord_geral_all %>% select(user_id, order_id, order_number
                                                     rec_fat = mean(reordered))
 
 
-base_ord_geral_all %>% filter(order_id == 144358)
+#base_ord_geral_all %>% filter(order_id == 144358)
 
 
 # base para K-Means com os campos descritos acima
@@ -174,28 +158,32 @@ base_k_user <- base_k_user_ord %>%
 
 
 # rodando K-means
-clust_kmean <- base_k_user[,c(2:ncol(base_k_user))] %>% scale() %>% hkmeans(k = 4)
+clust_kmean <- base_k_user[,c(2:ncol(base_k_user))] %>% 
+              scale() %>% 
+              hkmeans(k = 4)
 
 # Calculando os perfis dos clusters
 
 dados_clusters <- clust_kmean$centers
 
-clust_kmean$size
+#clust_kmean$size
 
 # A partir da análise é possível definir o perfil dos clusters, para poder dar um nome aos mesmos
-dados_clusters <- tibble(cluster = rownames(dados_clusters), nome_cluster = c("Novo_com_Potencial","Fiel_e_Recorrente","Compra_Quant","Churn")) %>% bind_cols(as_tibble(dados_clusters))
+dados_clusters <- tibble(cluster = rownames(dados_clusters), 
+                         nome_cluster = c("Novo_com_Potencial","Fiel_e_Recorrente","Compra_Quant","Churn")) %>% 
+  bind_cols(as_tibble(dados_clusters))
 
-(spider <- dados_clusters %>% ggRadar(aes(x = c(n_compras,
-                                     t_mean,
-                                     mean_prod_cart,
-                                     mean_peso_cart,
-                                     mean_rec_fat),
-                               facet = cluster), 
-                           interactive = F,
-                           size = 1.5,
-                           legend.position = "right"
-                           ))
-
+#(spider <- dados_clusters %>% ggRadar(aes(x = c(n_compras,
+#                                     t_mean,
+#                                     mean_prod_cart,
+#                                     mean_peso_cart,
+#                                     mean_rec_fat),
+#                               facet = cluster), 
+#                           interactive = F,
+#                           size = 1.5,
+#                           legend.position = "right"
+#                           ))
+#
 
 
 base_graf2 <- base_k_user %>%
@@ -208,68 +196,68 @@ base_graf_exp <- base_k_user %>%
 
 
 # Apresentando os dados dos clusters --------------------------------------
-base_graf_exp %>% select(-user_id) %>% group_by(cluster) %>% summarise(n_compras = mean(n_compras), 
-                                                                       t_mean = mean(t_mean), 
-                                                                       mean_prod_cart = mean(mean_prod_cart), 
-                                                                       mean_peso_cart = mean(mean_peso_cart), 
-                                                                       mean_rec_fat = mean(mean_rec_fat))
-
+#base_graf_exp %>% select(-user_id) %>% group_by(cluster) %>% summarise(n_compras = mean(n_compras), 
+#                                                                       t_mean = mean(t_mean), 
+#                                                                       mean_prod_cart = mean(mean_prod_cart), 
+#                                                                       mean_peso_cart = mean(mean_peso_cart), 
+#                                                                       mean_rec_fat = mean(mean_rec_fat))
+#
 
 # Exportando base para o Leo ----------------------------------------------
 
-base_graf_exp %>% write.csv("data\\base_graf_exp.csv")
+#base_graf_exp %>% write.csv("../output/base_graf_exp.csv")
 
 
 # Importando Base Leo e filtrando -----------------------------------------
 
-base_leo <- read.csv("data\\cl4.csv")
-
-base_leo <- base_leo %>% mutate(user_id = name) %>% select(user_id, cluster) %>% filter(cluster == 3) %>% tibble()
-
-base_leo %>% skim()
-
-
-# Encontrando a Interseção ------------------------------------------------
-
-intersect <- base_leo %>% inner_join(base_graf2, by = "user_id")
-
-intersect %>% select(-c(user_id,cluster.x)) %>% group_by(cluster.y) %>% summarise(n_compras = mean(n_compras), 
-                                                                       t_mean = mean(t_mean), 
-                                                                       mean_prod_cart = mean(mean_prod_cart), 
-                                                                       mean_peso_cart = mean(mean_peso_cart), 
-                                                                       mean_rec_fat = mean(mean_rec_fat))
-
+#base_leo <- read.csv("data/cl4.csv")
+#
+#base_leo <- base_leo %>% mutate(user_id = name) %>% select(user_id, cluster) %>% filter(cluster == 3) %>% tibble()
+#
+#base_leo %>% skim()
+#
+#
+## Encontrando a Interseção ------------------------------------------------
+#
+#intersect <- base_leo %>% inner_join(base_graf2, by = "user_id")
+#
+#intersect %>% select(-c(user_id,cluster.x)) %>% group_by(cluster.y) %>% summarise(n_compras = mean(n_compras), 
+#                                                                       t_mean = mean(t_mean), 
+#                                                                       mean_prod_cart = mean(mean_prod_cart), 
+#                                                                       mean_peso_cart = mean(mean_peso_cart), 
+#                                                                       mean_rec_fat = mean(mean_rec_fat))
+#
 
 # Gráfico de TREEMAP ------------------------------------------------------
 
 
   # basic treemap
-p <- base_ord_geral_all %>%
-          right_join(base_graf2 %>% select(user_id)) %>%
-          group_by(aisle, product_name) %>%
-          summarise(contagem = n()) %>%
-          arrange(desc(contagem)) %>%
-          data.frame() %>%
-          slice_head(n = 50) %>% 
-          treemap(base_ord_geral_all,
-                     index=c("aisle","product_name"),
-                     vSize="contagem",
-                     type="index",
-                     palette = "Set2",
-                     bg.labels=c("white"),
-                     align.labels=list(
-                       c("center", "center"), 
-                       c("right", "bottom")
-                     )
-        )
-
-  # make it interactive ("rootname" becomes the title of the plot):
-inter <- d3tree2(p, rootname = "Pricipais Produtos")
-inter
+#p <- base_ord_geral_all %>%
+#          right_join(base_graf2 %>% select(user_id)) %>%
+#          group_by(aisle, product_name) %>%
+#          summarise(contagem = n()) %>%
+#          arrange(desc(contagem)) %>%
+#          data.frame() %>%
+#          slice_head(n = 50) %>% 
+#          treemap(base_ord_geral_all,
+#                     index=c("aisle","product_name"),
+#                     vSize="contagem",
+#                     type="index",
+#                     palette = "Set2",
+#                     bg.labels=c("white"),
+#                     align.labels=list(
+#                       c("center", "center"), 
+#                       c("right", "bottom")
+#                     )
+#        )
+#
+#  # make it interactive ("rootname" becomes the title of the plot):
+#inter <- d3tree2(p, rootname = "Pricipais Produtos")
+#inter
   
-# Resultado: falha para rodar o k_means devido ao tamanho da base
-# Próximo passo
-# criar um sample da base para isso e também ver para Kendall e Pearson (análise e seguir)
+ #Resultado: falha para rodar o k_means devido ao tamanho da base
+ #Próximo passo
+ #criar um sample da base para isso e também ver para Kendall e Pearson (análise e seguir)
 
 
-brewer_pal(palette = "set1")
+#brewer_pal(palette = "set1")
